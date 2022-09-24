0432fd2518cbc8edde7348baba039d3625fd37c1
Kennedymuthuri <82339780+Kennedymuthuri@users.noreply.github.com>
2022-09-24 19:41:05 +0300

    Update connection.js

---
+++ backend/connection.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const bodyParser= require('body-parser');
+const cors= require('cors');
+const mongoose = require('mongoose');
+const PORT = 8081;
+const app= express();
+
